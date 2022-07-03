eb9e70a3b16c718d25cf05a5a06d537115fb533e
TuLuongg <107625316+TuLuongg@users.noreply.github.com>
2022-07-03 15:14:25 +0700

    Create APINeedTesting.java

---
+++ APITest/src/main/java/autotest/APINeedTesting.java
@@ -0,0 +1,14 @@
+package autotest;
+
+public abstract class APINeedTesting {
+	protected int codeResponse;
+	protected String messageResponse;
+	protected String dataResponse;
+	
+	public String creRequest(String... request) {
+		return null;
+	}
+	public void callAPI(String request) {
+		
+	}
+}
